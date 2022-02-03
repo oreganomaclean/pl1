abc1c08c6be222fdde2b6d5769c4828a0d03258e
oreganomaclean <91283582+oreganomaclean@users.noreply.github.com>
2022-02-03 11:35:19 -0500

    Update belgian.js

--- story/belgian.js
+++ story/belgian.js
@@ -1,20 +1,19 @@
-// The following example creates five accessible and
+// The following example creates five chocolate shop markers in Belgium and
 // focusable markers.
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: { lat: 34.84555, lng: -111.8035 },
   });
-  // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+  // Set LatLng and title text for the markers. The first marker (Neuhaus)
   // receives the initial focus when tab is pressed. Use arrow keys to
   // move between markers; press tab again to cycle through the map controls.
   const tourStops = [
-    [{ lat: 34.8791806, lng: -111.8265049 }, "Boynton Pass"],
-    [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
-    [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
-    [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
-    [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
-  ];
+    [{ lat: 50.84687753261257, lng: 4.3531066713300275 }, "Neuhaus"], 
+    [{ lat: 50.846625126831874, lng: 4.353738846454988 }, "Gourmet Chocolaterie Belge"], 
+    [{ lat: 50.847015139632866, lng: 4.354423215610558 }, "Godiva"], 
+    [{ lat: 50.8473606181687, lng: 4.351853659486694 }, "Elisabeth"],
+    [{ lat: 50.84751213131759, lng: 4.351931397330474 }, "Leonidas"], 
   // Create an info window to share between markers.
   const infoWindow = new google.maps.InfoWindow();
 
